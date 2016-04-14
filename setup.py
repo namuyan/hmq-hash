ca2d3a137f2fdb58d6667691d7b0bca63ac89e60
John@doe.com <FailCoin@failcoin.co.uk>
2016-04-14 22:04:56 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup, Extension
+
+hmq_hash_module = Extension('hmq_hash',
+                               sources = ['hmqmodule.cpp',
+                                          'sha3/blake.c',
+					  'sha3/bmw.c',
+					  'sha3/groestl.c',
+					  'sha3/jh.c',
+					  'sha3/keccak.c',
+					  'sha3/skein.c',
+					  'sha3/luffa.c',
+					  'sha3/cubehash.c',
+					  'sha3/shavite.c',
+					  'sha3/simd.c',
+					  'sha3/echo.c',
+					  'sha3/hamsi.c',
+#					  'sha3/hamsi_helper.c',
+					  'sha3/fugue.c',
+					  'sha3/shabal.c',
+					  'sha3/whirlpool.c',
+					  'sha3/sha2big.c',
+					  'sha3/haval.c',
+#					  'sha3/haval_helper.c',
+#					  'sha3/aes_helper.c',
+#					  'sha3/md_helper.c',
+					 ],
+                               include_dirs=['.', './sha3'])
+
+setup (name = 'hmq_hashs',
+       version = '1.0',
+       description = 'Bindings for proof of work/stake used by hmq',
+       ext_modules = [hmq_hash_module])    
